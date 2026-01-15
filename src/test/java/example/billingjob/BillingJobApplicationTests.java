b2a6fffe83f08a1258ecff5b30f3f67c09e1a602
mauriziofranco <mauriziofranco@astir.com>
2026-01-15 10:12:51 +0100

    initial commit

---
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -0,0 +1,116 @@
+package example.billingjob;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.junit.jupiter.api.BeforeEach;
+
+@SpringBatchTest
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
+class BillingJobApplicationTests {
+
+//    @Autowired
+//    private Job job;
+//
+//    @Autowired
+//    private JobLauncher jobLauncher;
+    
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+    
+    
+
+//    //FIRST EXECUTE METHOD VERSION TEST
+//    @Test
+//    void testJobExecution(CapturedOutput output) throws Exception {
+//        // given
+//        JobParameters jobParameters = new JobParameters();
+//
+//        // when
+//        JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+//
+//        // then
+//        Assertions.assertTrue(output.getOut().contains("processing billing information"));
+//        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+//    }
+    
+//    //SECOND EXECUTE METHOD VERSION TEST
+//    @Test
+//    void testJobExecution(CapturedOutput output) throws Exception {
+//        // given
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("input.file", "/some/input/file")
+//                .addString("file.format", "csv", false)
+//                .toJobParameters();
+//        // when
+//        JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+//        // then
+//        Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
+//        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+//    }
+    
+    @BeforeEach
+    public void setUp() {
+       this.jobRepositoryTestUtils.removeJobExecutions();
+    }
+    
+      //THIRD METHOD VERSION
+//    @Test
+//    void testJobExecution(CapturedOutput output) throws Exception {
+//        // given
+//        JobParameters jobParameters = new JobParametersBuilder()
+//            .addString("input.file", "/some/input/file")
+//            .toJobParameters();
+//
+//        // when
+//        // ** Update the following line:
+//        JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+//
+//        // then
+//        Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
+//        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+//    }
+    
+    @Test
+    void testJobExecution(CapturedOutput output) throws Exception {
+        // given
+        JobParameters jobParameters = 
+            this.jobLauncherTestUtils.getUniqueJobParametersBuilder()
+            .addString("input.file", "/some/input/file")
+            .toJobParameters();
+
+        // when
+        // ** Update the following line:
+        JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
